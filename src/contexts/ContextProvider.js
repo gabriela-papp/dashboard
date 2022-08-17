8f9e59535c1701c2b67e9723757ddaf9ccdb1868
Gabriela Papp <gpgabika@gmail.com>
2022-08-17 22:23:09 +0100

    themeSettings started

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,28 +1,55 @@
-import { createContext,useContext,useState } from "react";
+import { createContext, useContext, useState } from 'react'
 
-const StateContext = createContext();
+const StateContext = createContext()
 
-const initialState={
-    chat:false,
-    cart:false,
-    userProfile:false,
-    notification:false,
-    
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
 }
 
-export const ContextProvider =({children})=>{
-    const [activeMenu,setActiveMenu]=useState(true);
-    const [isClicked,setIsClicked]= useState(initialState)
-    const [screenSize,setScreenSize]=useState(undefined)
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true)
+  const [isClicked, setIsClicked] = useState(initialState)
+  const [screenSize, setScreenSize] = useState(undefined)
+  const [currentColor, setCurrentColor] = useState('#03C9D7')
+  const [currentMode, setCurrentMode] = useState('Light')
+  const [themeSetting, setThemeSetting] = useState(false)
 
-    const handleClick=(clicked)=>{
-        setIsClicked({...initialState,[clicked]:true})
-    }
+  const setMode = (e) => {
+    setCurrentMode(e.target.value)
+    localStorage.setItem('themeMode', e.target.value)
+  }
+  const setColor = (e) => {
+    setCurrentColor(e.target.value)
+    localStorage.setItem('colorMode', e.target.value)
+  }
 
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true })
+  }
 
-    return(
-        <StateContext.Provider value={{activeMenu,setActiveMenu,isClicked,setIsClicked,handleClick,screenSize,setScreenSize}}>{children}</StateContext.Provider>
-    )
+  return (
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+        currentColor,
+        currentMode,
+        setCurrentColor,
+        setCurrentMode,
+        themeSetting,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  )
 }
 
-export const useStateContext =()=>useContext(StateContext);
+export const useStateContext = () => useContext(StateContext)
